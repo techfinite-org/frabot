66055a19ea97f4afd25646ecc9fba318604d0802
kavi-TFS <146436408+Kavi-25@users.noreply.github.com>
2025-05-22 16:27:07 +0530

    Delete executor.py

--- executor.py
+++
@@ -1,9 +0,0 @@
-import frappe
-from robot import run
-
-
-def main():
-    frappe.init(site='claimgenie.local')
-    frappe.connect()
-    file = frappe.get_app_path('agarwals', 'tests', 'rough_work', 'test_get_cost_center.robot')
-    run(file)
